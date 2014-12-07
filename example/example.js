02a1de80a8206ec4acb69a7a0b7fc42cb960c78f
troy betz <troybetz@gmail.com>
2014-12-07 07:25:12 -0600

    add example styling & controls

--- example/example.js
+++ example/example.js
@@ -10,6 +10,60 @@ var YouTube = require('../');
 
 var player = new YouTube('youtube-embed');
 
+/**
+ * Event handlers
+ */
+
 player.on('ready', function() {
-  player.play();
+  console.log('READY');
+  console.log('player: ', player);
+
+  /**
+   * Bind button controls
+   */
+  
+  bindPlay(player);
+  bindPause(player);
+  bindDestroy(player);
 });
+
+player.on('play', function() {
+  console.log('PLAYING');
+});
+
+player.on('pause', function() {
+  console.log('PAUSED');
+});
+
+player.on('end', function() {
+  console.log('ENDED');
+});
+
+/**
+ * Player controls
+ */
+
+
+function bindPlay(player) {
+  var play = document.getElementById('play');
+
+  play.addEventListener('click', function() {
+    player.play();
+  });
+}
+
+function bindPause(player) {
+  var pause = document.getElementById('pause');
+
+  pause.addEventListener('click', function() {
+    player.pause();
+  });
+}
+
+function bindDestroy(player) {
+  var destroy = document.getElementById('destroy');
+
+  destroy.addEventListener('click', function() {
+    player.destroy();
+  });
+}
